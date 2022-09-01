57088314eb8157a23e1a28be61799ee88d187fbc
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-01 09:25:47 +0000

    [Add]ordersコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,33 @@
 Rails.application.routes.draw do
 
   namespace :public do
+    get 'orders/new'
+    post 'orders/confirm'
+    get 'orders/complete'
+    get 'orders/index'
+    get 'orders/show'
+  end
+
+  namespace :public do
     get 'cart_items/index'
   end
+
   namespace :public do
     get 'customers/show'
     get 'customers/edit'
     get 'customers/unsubscribe'
   end
+
   namespace :public do
     get 'items/index'
     get 'items/show'
   end
+
   namespace :public do
     get 'homes/top'
     get 'homes/about'
   end
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
