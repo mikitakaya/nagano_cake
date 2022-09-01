f6ea8acb2371228c47b35a0675ecabaf7747c89b
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-01 10:59:19 +0000

    [Update]public/homesのルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,27 @@
 Rails.application.routes.draw do
 
   # public/orders
-  post 'orders/confirm'
-  get 'orders/complete'
+  post 'orders/confirm' => "orders#confirm", as: "confirm"
+  get 'orders/complete' => "orders#complete", as: "complete"
   resources :orders, only: [:new, :index, :show, :create]
 
   # public/cart_items
-  delete 'cart_items/destroy_all'
+  delete 'cart_items/destroy_all' => "cart_items#destroy_all", as: "destroy_all"
   resources :cart_items, only: [:index, :update, :destroy, :create]
 
   # public/customers
-  get 'customers/my_page' => 'customers#show'
-  get 'customers/information/edit' => 'customers#edit'
-  patch 'customers/information' => 'customers#update'
-  get 'customers/unsubscribe'
-  patch 'customers/withdraw'
+  get 'customers/my_page' => 'customers#show', as: "show"
+  get 'customers/information/edit' => 'customers#edit', as: "edit"
+  patch 'customers/information' => 'customers#update', as: "update"
+  get 'customers/unsubscribe' => "customers#unsubscribe", as: "unsubscribe"
+  patch 'customers/withdraw' => "customers#withdraw", as: "withdraw"
 
   # public/items
   resources :items, only: [:index, :show]
 
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+  # public/homes
+  root to: 'homes#top'
+  get 'about' => "homes#about", as: "about"
 
   # 顧客用
   # URL /customers/sign_in ...
