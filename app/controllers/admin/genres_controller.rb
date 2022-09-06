bcaa4cd2f4134c22d6a89e2b87ec886be9928826
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-06 09:18:06 +0000

    [Add]ジャンル一覧画面を作成中

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,6 +3,14 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
+   # Viewのform_withへ渡すための「空のモデル」を生成する。
+   @newgenre = Genre.new
+   # データを受け取り新規登録するためのインスタンス作成
+   @genre = Genre.new(genre_params)
+   # データをデータベースに保存する
+   @genre.save
+   # ジャンル登録後、ジャンル一覧画面にリダイレクト
+   redirect_to admin_genres_path
   end
 
   def edit
