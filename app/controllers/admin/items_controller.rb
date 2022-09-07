4a1bcbf6b368a3f1886e9c1434489f3051b7de7d
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-07 11:19:30 +0000

    [Update]商品の新規登録機能を作成中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,15 +9,30 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+   # データを受け取り新規登録するためのインスタンス作成
+   @item = Item.new(item_params)
+   # データをデータベースに保存する
+   @item.save
+   # 商品詳細画面にリダイレクト
+   redirect_to admin_item_path(@item.id)
   end
 
   def show
+   # レコードを1件だけ取得
+   @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  # 商品データのストロングパラメータ
+  private
+
+  def item_params
+   params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 
 end
