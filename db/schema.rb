ea4a20fdea32349c897a2b2798d27312f20d0799
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-08 09:16:50 +0000

    [Update]商品詳細画面を作成中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_055138) do
+ActiveRecord::Schema.define(version: 2022_09_08_074730) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2022_08_31_055138) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
+    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
