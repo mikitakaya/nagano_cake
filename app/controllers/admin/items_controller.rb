ed51f1a1f655a5a40ddda133b0d5170ca14fb861
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-13 09:52:41 +0000

    [Update]商品一覧画面を作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,10 +1,13 @@
 class Admin::ItemsController < ApplicationController
   def index
+   # itemsテーブルに保存されている全てのデータを取得する
+   @items = Item.page(params[:page])
   end
 
   def new
    # Viewのform_withへ渡すための「空のモデル」を生成する
    @item = Item.new
+   # genresテーブルに保存されている全てのデータを取得する
    @genres = Genre.all
   end
 
@@ -25,12 +28,14 @@ class Admin::ItemsController < ApplicationController
   def edit
    # レコードを1件だけ取得
    @item = Item.find(params[:id])
+   # genresテーブルに保存されている全てのデータを取得する
    @genres = Genre.all
   end
 
   def update
    # レコードを1件だけ取得
    @item = Item.find(params[:id])
+   # binding.pry
    @item.update(item_params)
    # 商品詳細画面にリダイレクト
    redirect_to admin_item_path(@item.id)
