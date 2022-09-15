7e3796364aea0242c574bd0eabee26edaa5f3e51
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-15 03:04:28 +0000

    [Update]管理者トップページを作成

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
+   # ordersテーブルに保存されている全てのデータを取得する
+   @orders = Order.page(params[:page])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,3 @@
 class Order < ApplicationRecord
+ enum order_status: { wait: 0, deposited: 1, make: 2, preparation: 3, sent: 4 }
 end
