2976dfa2b4f1d362fb8f02ec6e271c30fb5592bd
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-15 11:12:27 +0000

    [Update]public/top画面を作成中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,10 +5,15 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+   # レコードを1件だけ取得
+   @order = Order.find(params[:id])
+   @order.update(order_params)
+   # 注文履歴詳細にリダイレクト
+   redirect_to admin_order_path(@order.id)
   end
 
   private
-  # ストロングパラメータ
+  # 注文データのストロングパラメータ
   def order_params
    params.require(:order).permit(:order_status, :crafting_status)
   end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,3 @@
 class OrderDetail < ApplicationRecord
+ enum crafting_status: { impossible: 0, wait: 1, make: 2, manufactured: 3 }
 end
