dce1abd096fafeceb81834c5ac2f1695d4a5ead3
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-20 10:15:42 +0000

    [Update]顧客の退会確認画面を作成、トップページのレスポンシブ対応を調整

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -23,6 +23,15 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdraw
+   # 現在ログインしているcustomer
+   @customer = current_customer
+   # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+   @customer.update(is_deleted: true)
+   # すべてのセッション情報を削除
+   reset_session
+   flash[:notice] = "退会処理を実行いたしました"
+   # トップ画面へリダイレクト
+   redirect_to root_path
   end
 
   private
