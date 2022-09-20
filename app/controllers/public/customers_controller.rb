2438b077bd876986491c417f622032dd4c62dc79
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-20 07:24:11 +0000

    [Update]マイページを作成中

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,7 @@
 class Public::CustomersController < ApplicationController
   def show
+   # 現在ログインしているcustomer
+   @customer = current_customer
   end
 
   def edit
@@ -14,4 +16,9 @@ class Public::CustomersController < ApplicationController
   def withdraw
   end
 
+  private
+  # 会員データのストロングパラメータ
+  def customer_params
+   params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+  end
 end
