c55b2c0cb96ad446cee6a8de37170d05245eae0b
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-09-21 07:15:48 +0000

    [Update]publicの配送先一覧を作成中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,17 +1,41 @@
 class Public::AddressesController < ApplicationController
   def index
+   # Viewのform_withへ渡すための「空のモデル」を生成する
+   @address = Address.new
+   # 全ての配送先データを取得する
+   @addresses = Address.all
   end
 
   def edit
+   # データレコードを１件取得
+   @address = Address.find(params[:id])
   end
 
   def create
+   # データを受け取り新規登録するためのインスタンス作成
+   @address = Address.new(address_params)
+   # データをデータベースに保存する
+   @address.save
+   # 配送先新規登録後、配送先登録／一覧にリダイレクト
+   redirect_to addresses_path
   end
 
   def update
   end
 
   def destroy
+   # データレコードを１件取得
+   @address = Address.find(params[:id])
+   # データレコードを削除
+   @address.destroy
+   # 配送先新規登録後、配送先登録／一覧にリダイレクト
+   redirect_to addresses_path
+  end
+
+  private
+  # 配送先データのストロングパラメータ
+  def address_params
+   params.require(:address).permit(:postal_code, :address, :name)
   end
 
 end
